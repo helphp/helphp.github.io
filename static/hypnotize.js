d489a798a9b570c72d4ebcd78ef7675b91c8dc90
helphp <122244726+helphp@users.noreply.github.com>
2024-09-22 20:56:14 -0600

    Update hypnotize.js

--- static/hypnotize.js
+++ static/hypnotize.js
@@ -71,4 +71,14 @@ class Hypnotize {
 
 document.addEventListener("DOMContentLoaded", () => {
     const hypnotize = new Hypnotize();
+
+            const divText = document.getElementById('github-text')
+            divText.classList.remove('github-text-off');
+            divText.classList.add('github-text-on');
+            messagesDisplayed = true;
+
+        document.getElementById('github-text-span').innerHTML = 'If you lived<br>here, you'd<br>already be<br>home'
+    }
+    setTimeout(display_message, 5000);
+}
 });
